5e81da25feef435878987d50abfc8754485d9ff2
Erik <bierszackerix@gmail.com>
2021-08-16 10:26:50 -0500

    Cambios recientes en el traspaso de datos al formulario

--- source/routes/links.js
+++ source/routes/links.js
@@ -7,17 +7,17 @@ module.exports = app => {
     const connection = database(); 
     app.get('/', (req, res) => {
         connection.query(`SELECT Id_Categorias, imagen_producto, producto, descripcion, costo FROM categorias`, (err, result) => {
-            console.log(result);
+            // console.log(result);
             res.render('main', {
                 catalogo: result
             });
         });
     });
 
-    app.get('/formulario.ejs/:Id', (req, res) => {
+    app.get('/formulario.ejs/:Id_Categorias', (req, res) => {
         const connection = database();
-        const {Id} = req.query.Id_Categorias;
-        connection.query(`SELECT * FROM categorias WHERE Id_Categorias = ?`, Id, (err, result) =>{
+        const Id_Categorias = req.params.Id_Categorias;
+        connection.query(`SELECT * FROM categorias WHERE Id_Categorias = '${Id_Categorias}'`, (err, result) =>{
             console.log(result);
             res.render('formulario', {
                 selectProduct: result
@@ -28,7 +28,7 @@ module.exports = app => {
     app.get('/marvel.ejs', (req, res) => {
         const connection = database();
         connection.query(`SELECT * FROM categorias WHERE marca = 'Marvel'`, (err, result) => {
-            console.log(result);
+            // console.log(result);
             res.render('marvel', {
                 marvel : result
             });
@@ -38,7 +38,7 @@ module.exports = app => {
     app.get('/dc.ejs' , (req, res) => {
         const connection = database();
         connection.query(`SELECT * From categorias WHERE marca = 'Dc'`, (err, result) => {
-            console.log(result);
+            // console.log(result);
             res.render('dc', {
                 dc: result
             });
@@ -48,7 +48,7 @@ module.exports = app => {
     app.get('/anime.ejs', (req, res) => {
         const connection = database()
         connection.query(`SELECT * FROM categorias WHERE marca = 'Anime'`, (err, result) => {
-            console.log(result);
+            // console.log(result);
             res.render('anime', {
                 anime: result
             });
